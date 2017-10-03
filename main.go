5f11c2c9fc587c3f30c8250221454b3b537cc701
Brice Colucci <brice@skale-5.com>
2017-10-03 18:00:31 +0200

    Format. Change default settings. Add .gitignore file.

--- main.go
+++ main.go
@@ -11,19 +11,19 @@ type Path struct {
 }
 
 type Map struct {
-	w, h	  int
-	paths 	[]Path
+	w, h  int
+	paths []Path
 }
 
 func NewMap(w int) *Map {
-	return &Map{w:w, h:w+1}
+	return &Map{w: w, h: w + 1}
 }
 
 func (m *Map) BuildPaths(nbPaths int) {
 	m.paths = make([]Path, nbPaths)
 	r := rand.New(rand.NewSource(time.Now().Unix()))
-	for i:= 0; i < nbPaths; i += 1 {
-		p := Path{values:make([]int, m.w)}
+	for i := 0; i < nbPaths; i += 1 {
+		p := Path{values: make([]int, m.w)}
 		p.values[0] = int(m.h/2) * m.w
 		for i := 1; i < m.w; i += 1 {
 			if r.Intn(2) == 0 {
@@ -37,32 +37,32 @@ func (m *Map) BuildPaths(nbPaths int) {
 }
 
 func (m *Map) Print() {
-	l := m.w*m.h
+	l := m.w * m.h
 	var found bool
 	for i := 0; i < l; i += 1 {
 		if i%m.w == 0 {
 			fmt.Printf("\n")
 		}
 		found = false
-		Loop:
-			for _, p := range m.paths {
-				for _, k := range p.values {
-					if k == i {
-						found = true
-						break Loop
-					}
+	Loop:
+		for _, p := range m.paths {
+			for _, k := range p.values {
+				if k == i {
+					found = true
+					break Loop
 				}
 			}
+		}
 		if found {
-			fmt.Print("[X]")
+			fmt.Print("X")
 		} else {
-			fmt.Print("[ ]")
+			fmt.Print(".")
 		}
 	}
 }
 
 func main() {
-	m := NewMap(66)
-	m.BuildPaths(10)
+	m := NewMap(250)
+	m.BuildPaths(20)
 	m.Print()
 }
